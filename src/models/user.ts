319300d8ab892e4302bf211091739de1497d8e71
Dmitry Sternyaev <dimastv@yandex.com>
2021-04-30 11:41:48 +0300

    Bug fix with emoji url

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,6 +2,7 @@ import { model, Schema, Types } from "mongoose";
 import { PROSTAVA, CODE } from "../constants";
 import { UserDocument, UserModel } from "../types";
 import emojiToolkit from "emoji-toolkit";
+import emojiUnicode from "emoji-unicode";
 
 const PersonalDataSchema = new Schema(
     {
@@ -39,13 +40,15 @@ UserSchema.virtual("user_link").get(function (this: UserDocument) {
     return `tg://user?id=${this.user_id}`;
 });
 UserSchema.virtual("user_photo").get(function (this: UserDocument) {
-    if (this.personal_data.emoji) {
-        return emojiToolkit
-            .toImage(this.personal_data.emoji)
-            ?.match(/(?<=src=")[^"]+/)[0]
-            ?.replace("/32/", "/128/");
+    const emojiImageUrl = emojiToolkit.toImage(this.personal_data?.emoji)?.match(/(?<=src=")[^"]+/);
+    if (emojiImageUrl?.length) {
+        return emojiImageUrl[0]?.replace("/32/", "/128/");
     }
-    return undefined;
+    const emojiCode = emojiUnicode(this.personal_data?.emoji);
+    if (emojiCode && !emojiCode.includes(" ")) {
+        return `https://cdn.jsdelivr.net/joypixels/assets/6.5/png/unicode/128/${emojiCode}.png`;
+    }
+    return "https://cdn.jsdelivr.net/joypixels/assets/6.5/png/unicode/128/1f921.png";
 });
 
 export const UserCollection = model<UserDocument, UserModel>(PROSTAVA.COLLECTION.USER, UserSchema);
