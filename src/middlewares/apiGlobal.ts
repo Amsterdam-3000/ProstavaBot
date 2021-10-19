b54cd5e37d19ab87da94cb73325e745ab92165a7
Dmitry Sternyaev <dimastv@yandex.com>
2021-10-19 18:43:57 +0300

    Add Cors Headers

--- src/middlewares/apiGlobal.ts
+++ src/middlewares/apiGlobal.ts
@@ -9,7 +9,7 @@ morgan.token("req-headers", function getReqHeaders(req) {
 export class ApiGlobalMiddleware {
     static addBodyToRequestFromUrl = urlencoded({ extended: false });
     static addBodyToRequestFromJson = json();
-    static addCorsHeaderToResponse = cors();
+    static addCorsHeaderToResponse = cors({ allowedHeaders: ["Content-Type", "Authorization"] });
     static addLoggingRequest = morgan(
         ":remote-addr - :remote-user [:date[clf]]\n" + ":method :url HTTP/:http-version :status\n" + ":req-headers\n"
     );
