2ac5a8d902d5439380bb215dd2eb3197180cc6db
Dmitry Sternyaev <dimastv@yandex.com>
2022-12-11 15:22:01 +0600

    Heroku22 create-react-app buildpack

---
+++ heroku-start.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "build"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "build/index.html"));
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port: ", port);
+});
